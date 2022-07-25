d7d6ef96fff6f9b1bbb10cefc9347403e50690d1
Ana Luiza Magalhães <anaslmagalhaes@gmail.com>
2022-07-24 21:26:49 -0300

    Melhorada a legibilidade do código

--- anel.c
+++ anel.c
@@ -11,38 +11,29 @@ int main(int argc, char *argv[]){
    
 
         ID = rank;
-        if(rank == 0){
-            predecessor = size - 1;
-        } else{
-            predecessor = rank - 1;
-        }
+        predecessor = (rank == 0) ? (size - 1) : (rank - 1);
 
-        if(rank == size - 1){
-            successor = 0; 
-        } else {
-            successor = rank + 1;
-        }
-
+        successor = (rank == size - 1) ? 0 : (rank + 1);
 
         int current_leader,
-            is_leader,
+            is_leader = 0,
             leader_elected = 0,
             message,
-            st_value;
+            st_value = 0;
 
         if(rank == 0){
             message = ID;
-            MPI_Send(&message, 1, MPI_INT, successor, 0, MPI_COMM_WORLD);
+            MPI_Send(&message, 1, MPI_INT, successor, 0, MPI_COMM_WORLD); // Iniciando eleição enviando ID do processo 0
         }
         while(!leader_elected){
-            MPI_Recv(&message, 1, MPI_INT, predecessor, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+            MPI_Recv(&message, 1, MPI_INT, predecessor, MPI_ANY_TAG, MPI_COMM_WORLD, &st); //Processo recebe mensagem do processo antecessor
 
-            printf("Sou o processo %d, recebi o número %d do processo %d\n", ID, message, predecessor);
+            printf("Sou o processo %d, recebi o ID %d do processo %d\n", ID, message, predecessor);
             fflush(stdout);
 
             st_value = st.MPI_TAG;
 
-            if(st_value == 2){
+            if(st_value == 200){
                 leader_elected = 1;
                 current_leader = message;
             } else {
@@ -50,29 +41,19 @@ int main(int argc, char *argv[]){
                     message = ID;
                 } else if(message == ID){
                     is_leader = 1;
-                    printf("Sou o processo %d e sou o lider!\n", rank);
+                    printf("O processo %d foi eleito lider!\n", rank);
                     fflush(stdout);
 
-                    st_value = 2;
+                    st_value = 200;
                 }
-            }
-            MPI_Send(&message, 1, MPI_INT, successor, st_value, MPI_COMM_WORLD);
+               
+            }   
+            MPI_Send(&message, 1, MPI_INT, successor, st_value, MPI_COMM_WORLD); //Processo repassa mensagem para o processo sucessor
             printf("ID: %d, número do processo repassado: %d\n", ID, message);
             fflush(stdout);
         }
         MPI_Barrier(MPI_COMM_WORLD);
-
-        if(is_leader == 0 ){
-            printf("\nMPI_RANK: %d, ID: %d, LIDER: FALSE\n", rank, ID);
-            fflush(stdout);
-        } else if (is_leader == 1){
-    
-            printf("\nMPI_RANK: %d, ID: %d, LIDER: TRUE\n", rank, ID);
-            fflush(stdout);
-        }
-
-   
-
+        
     MPI_Finalize();
     return 0;
 }
